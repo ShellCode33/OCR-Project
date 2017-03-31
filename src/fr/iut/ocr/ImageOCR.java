44ce6f565364b96b068e1cf4747ac3d8d629d49e
tbeguey <theo.beguey@etu.u-bordeaux.fr>
2017-03-31 13:01:06 +0200

    moyenne

--- src/fr/iut/ocr/ImageOCR.java
+++ src/fr/iut/ocr/ImageOCR.java
@@ -1,6 +1,8 @@
 package fr.iut.ocr;
 
+import ij.IJ;
 import ij.ImagePlus;
+import ij.process.ImageProcessor;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -15,5 +17,24 @@ public class ImageOCR {
 
     public ImageOCR(String filename, String expected_value) {
         img = new ImagePlus(filename);
+    }
+
+
+    public double averageImage(ImageProcessor ip){
+        byte[] pixels = (byte[]) ip.getPixels(); // Notez le cast en byte ()
+        int height = ip.getHeight();
+        int width = ip.getWidth();
+
+        int sum = 0;
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                sum += pixels[j * width + i] & 0xff;
+            }
+        }
+
+        sum = sum / (width*height);
+
+        return sum;
     }
 }
