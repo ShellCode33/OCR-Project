d57e825be6b40811a4957f9cf33a9276cba2469b
ShellCode33 <clement.fleury33@gmail.com>
2017-03-31 09:13:44 +0200

    Initial commit

---
+++ src/fr/iut/ocr/PluginOCR.java
@@ -0,0 +1,19 @@
+package fr.iut.ocr;
+
+import ij.ImagePlus;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.ImageProcessor;
+
+/**
+ * Created by shellcode on 3/31/17.
+ */
+public class PluginOCR implements PlugInFilter {
+
+    public int setup(String s, ImagePlus imagePlus) {
+        return 0;
+    }
+
+    public void run(ImageProcessor imageProcessor) {
+
+    }
+}
